998fd784459ee98fea92cf4c83a3fe2533d203de
DDorka02 <dobszaydorka@gmail.com>
2024-02-16 08:42:43 +0100

    js

---
+++ index.js
@@ -0,0 +1,6 @@
+udvozles('Géza') /*a hoi szting felemelés, a függvényeket és változókat dektaráljuk a hatókör=skop(ahonnan elérhető a metódus)*/
+
+function udvozles(nev) {
+    console.log("Jó reggelt " + nev +"!");
+  }
+  
